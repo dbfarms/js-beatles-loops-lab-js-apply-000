8cb4676acaa2c48bdb804b5acd67c4adcb5ee900
Matthew Hunger <matt.hunger@gmail.com>
2017-09-20 11:26:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var playswhat = []
+  var string 
   for (var i = 0; i < musicians.length; i++) {
     string = `${musicians[i]} plays ${instruments[i]}`
     playswhat.push(string)
